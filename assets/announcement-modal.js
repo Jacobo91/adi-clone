2637a7bf57199cf9c5e2f41e6cc6e89c15744951
jacobo91 <jacobovelasquez1991@gmail.com>
2023-09-02 12:54:06 -0500

    finish adding announcement modal

--- assets/announcement-modal.js
+++ assets/announcement-modal.js
@@ -1,16 +1,27 @@
 const openAnnouncementBar = document.querySelector('.utility-bar');
 const closeAnnouncementBar = document.querySelector('.close-announcement-modal-button');
 const announcementModal = document.querySelector('.announcement-modal');
+const announcementModalContent = document.querySelector('.announcement-modal--content');
+const body = document.querySelector('.my-body');
 
 openAnnouncementBar.addEventListener('click', () => {
     if (announcementModal.classList.contains('inactive')) {
         announcementModal.classList.remove('inactive');
         announcementModal.classList.add('active');
     }
+    body.classList.add('disable-scroll')
 })
 
 closeAnnouncementBar.addEventListener('click', () => {
     if (announcementModal.classList.contains('active')) {
+        announcementModal.classList.remove('active');
+        announcementModal.classList.add('inactive');
+    }
+    body.classList.remove('disable-scroll')
+})
+
+document.addEventListener('click', (e) => {
+    if (announcementModal.classList.contains('active') && !announcementModalContent.contains(e.target) && !openAnnouncementBar.contains(e.target)) {
         announcementModal.classList.remove('active');
         announcementModal.classList.add('inactive');
     }
