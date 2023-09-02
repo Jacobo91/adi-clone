b0fb20b096f100e332fa37664c0a990df616fc2f
jacobo91 <jacobovelasquez1991@gmail.com>
2023-09-01 21:39:04 -0500

    add announcement modalanimation

--- assets/announcement-modal.js
+++ assets/announcement-modal.js
@@ -1,5 +1,17 @@
-const announcementBar = document.querySelector('.utility-bar');
+const openAnnouncementBar = document.querySelector('.utility-bar');
+const closeAnnouncementBar = document.querySelector('.close-announcement-modal-button');
+const announcementModal = document.querySelector('.announcement-modal');
 
-announcementBar.addEventListener('click', () => {
-    console.log("clicking announcement bar")
+openAnnouncementBar.addEventListener('click', () => {
+    if (announcementModal.classList.contains('inactive')) {
+        announcementModal.classList.remove('inactive');
+        announcementModal.classList.add('active');
+    }
+})
+
+closeAnnouncementBar.addEventListener('click', () => {
+    if (announcementModal.classList.contains('active')) {
+        announcementModal.classList.remove('active');
+        announcementModal.classList.add('inactive');
+    }
 })
